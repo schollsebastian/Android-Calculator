222a5614eef0f68e7575975ca1eea2bf77fa848e
schollsebastian <scholl.sebastian@gmx.at>
2021-11-29 13:42:07 +0100

    Implement typeNumber

--- app/src/main/java/at/htl/calculator/MainActivity.kt
+++ app/src/main/java/at/htl/calculator/MainActivity.kt
@@ -2,10 +2,17 @@ package at.htl.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun typeNumber(view: View) {
+        output.text = (view as Button).text
     }
 }
